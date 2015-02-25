b4344874e7de8e2491d2a537211133b54935be4e
Justin R. Porter <justinrporter@gmail.com>
2015-02-25 13:12:31 -0600

    Make image smaller, implement contact, etc.

--- app/about/about.js
+++ app/about/about.js
@@ -4,7 +4,11 @@ angular.module('myApp.about', ['ngRoute'])
 
 .config(['$routeProvider', function($routeProvider) {
   $routeProvider.when('/about', {
-    templateUrl: 'about/about.html',
+    templateUrl: 'about/services.html',
+    controller: 'AboutCtrl'
+  });
+  $routeProvider.when('/contact', {
+    templateUrl: 'about/contact.html',
     controller: 'AboutCtrl'
   });
 }])
