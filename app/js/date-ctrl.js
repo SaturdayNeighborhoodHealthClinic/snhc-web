1b08bd30c33f57556e87b5d3cdd5eccdc0ba1b92
Justin R. Porter <justinrporter@gmail.com>
2015-02-28 20:57:59 -0600

    Add demangling code for clinic dates.

--- app/js/date-ctrl.js
+++ app/js/date-ctrl.js
@@ -9,3 +9,16 @@ myApp.controller('DateListCtrl', ['$scope', '$http', function($scope, $http) {
     $scope.phones = data.feed.entry;
   });
 }]);
+
+myApp.filter('unmangle', function() {
+  return function(input) {
+    input = input || '';
+    // Just hack this and hope to God google never changes this API. When they do, maybe
+    // they'll provide better access to this info?
+    var nbsp = input.indexOf("&nbsp");
+    var begin = 5;
+    return input.slice(begin,nbsp);
+  };
+})
+
+  
