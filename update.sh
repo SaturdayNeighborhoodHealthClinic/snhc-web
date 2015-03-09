e1c729d3c1be08e914d921f620b73ed9e80e12b4
Justin R. Porter <justinrporter@gmail.com>
2015-03-09 15:06:36 -0500

    Updating now creates a log.

--- update.sh
+++ update.sh
@@ -1,3 +1,8 @@
+exec &> update.tmp
+
+echo -n "UPDATE:"
+date
+
 rm -rf $( ls | grep -v bower_components | grep -v update.log )
 wget --no-check-certificate https://github.com/justinrporter/snhc-web/archive/master.zip
 unzip master
@@ -5,3 +10,5 @@ rm -r master
 mv snhc-web-master/app/* .
 mv snhc-web-master/update.sh .
 rm -rf snhc-web-master
+
+cat update.tmp >> update.log
