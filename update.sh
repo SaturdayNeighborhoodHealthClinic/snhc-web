6760e5b35cfa333dcb79d31709f59439258e2bf1
Justin R. Porter <justinrporter@gmail.com>
2015-03-09 15:18:17 -0500

    Exclude update.latest from update.sh removal of current files.

--- update.sh
+++ update.sh
@@ -5,7 +5,7 @@ cat update.latest >> update.log
 echo -n "UPDATE:"
 date
 
-rm -rf $( ls | grep -v bower_components | grep -v update.log )
+rm -rf $( ls | grep -v bower_components | grep -v update.log | grep -v update.latest )
 wget --no-check-certificate https://github.com/justinrporter/snhc-web/archive/master.zip
 unzip master
 rm -r master
