546abbc638a1522e4d9cb16efa1a9f5877ea72d0
Justin R. Porter <justinrporter@gmail.com>
2015-03-29 11:09:24 -0700

    Modify calendar DI to use the google API the right way.

--- app/js/date-ctrl.js
+++ app/js/date-ctrl.js
@@ -4,9 +4,25 @@
 
 var dateController = angular.module('myApp.datelist', []);
 
-myApp.controller('DateListCtrl', ['$scope', '$http', function($scope, $http) {
-  $http.get('https://www.google.com/calendar/feeds/7eie7k06g255baksfshfhp0m28%40group.calendar.google.com/public/basic?orderby=starttime&sortorder=ascending&futureevents=true&alt=json&singleevents=true&max-results=30').success(function(data) {
-    $scope.phones = data.feed.entry;
+myApp.controller('DateListCtrl', ['$scope', '$http', '$window', function($scope, $http, $window) {
+
+  var google_key = 'AIzaSyDllIaMvMrMYrTxHRTzR9R9Ze23-Cf8iRU'; // public-access API key
+  var calendarid = '7eie7k06g255baksfshfhp0m28%40group.calendar.google.com'; // calendar id from Google
+
+  $http({
+    url: "https://www.googleapis.com/calendar/v3/calendars/" + calendarid+ '/events',
+    method: "GET",
+    params: { key : google_key,
+              "singleEvents" : true,
+              "timeMin" : new Date(),
+              "orderBy" : "startTime"  }
+  }).success(function(data, status, headers, config) {
+
+    $scope.data = data;
+
+  }).error(function(data, status, headers, config) {
+    $scope.status = status;
+    $window.alert("Whoops! We failed to contact the Google Calendar. Response was "+status+". Sorry!");
   });
 }]);
 
