398f778be0735cc80b574ab320428c511095ad68
Justin R. Porter <justinrporter@gmail.com>
2015-03-29 09:49:41 -0700

    Pull calendar data using RESTful API and new key setup for calendar.

--- app/js/cal.js
+++ app/js/cal.js
@@ -4,16 +4,24 @@
 
 var calController = angular.module('myApp.calcontrol', []);
 
-myApp.controller('CalCtrl', ['$scope', '$document', function($scope, $document) {
+myApp.controller('CalCtrl', ['$scope', '$document', '$http', '$window', function($scope, $document, $http, $window) {
 
-  $(document).ready(function() {
+  var mykey = 'AIzaSyDllIaMvMrMYrTxHRTzR9R9Ze23-Cf8iRU'; // public-access API key
+  var calendarid = '7eie7k06g255baksfshfhp0m28%40group.calendar.google.com'; // calendar id from Google
 
-    // page is now ready, initialize the calendar...
+  $http({
+    url: "https://www.googleapis.com/calendar/v3/calendars/" + calendarid+ '/events?key=' + mykey,
+    method: "GET"
+  }).success(function(data, status, headers, config) {
+
+    $scope.data = data;
 
     $('#calendar').fullCalendar({
         // put your options and callbacks here
     })
-
-  })
+  }).error(function(data, status, headers, config) {
+    $scope.status = status;
+    $window.alert("Whoops! We failed to contact Google Calendar. Response was "+status+". Sorry!");
+  });
 
 }]);
