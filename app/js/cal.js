1d843aecfdabbea93c7b7de41ddb86e1ba5efda6
Justin R. Porter <justinrporter@gmail.com>
2015-04-06 15:34:27 -0500

    Begin implementing firebase interaction on signup list.

--- app/js/cal.js
+++ app/js/cal.js
@@ -7,10 +7,13 @@ angular.module('myApp.calcontrol', ['myApp.calService', 'firebase'])
   .service('fbRef', function(fbURL) {
     return new Firebase(fbURL)
   })
-  .controller('CalCtrl', function($scope, $window, $firebaseObject, fbRef, cal_http ) {
-
+  .service('fbo', function( $firebaseObject, fbRef ) {
+    return $firebaseObject(fbRef.child("events"));
+  })
+  .controller('CalCtrl', function($scope, $window, cal_http ) {
     cal_http
     .success(function(data, status, headers, config) {
+      $scope.cal_events = [];
       for ( var index in data.items ) {
         // data.times are "events" resources.
         // API doc: https://developers.google.com/google-apps/calendar/v3/reference/events#resource
@@ -20,20 +23,29 @@ angular.module('myApp.calcontrol', ['myApp.calService', 'firebase'])
         var event_properties = { title : data.items[index].summary,
                                  start : data.items[index].start.dateTime,
                                  end : data.items[index].end.dateTime,
-                                 color : "purple",
                                  id : data.items[index].id
-                                // rendering : background"
                                 };
+        $scope.cal_events.push( event_properties );
       }
-      $scope.events = event_list;
     })
     .error(function(data, status, headers, config) {
       $window.alert("Whoops! We failed to contact the Google Calendar. Response was "+status+". Sorry!");
     });
+  })
+  .controller('FirebaseCtrl', function($scope, $firebaseArray, fbRef ){
+    console.log( $scope.event );
+    $scope.firebase_event = $firebaseArray(fbRef.child("events").child($scope.event.id));
 
-    // download the data into a local object
-    // var syncObject = $firebaseObject(fbRef);
+    $scope.open_clinical = true;
+    $scope.open_preclinical = true;
+    $scope.open_attending = true;
 
-    // synchronize the object with a three-way data binding
-    // syncObject.$bindTo($scope, "data");
+    // $scope.volunteer = function( gcal_ev ){
+    //   firebase_event.$loaded().then(function() {
+    //     console.log( firebase_event.$getRecord('preclinical'));
+    //     console.log( firebase_event.$getRecord('preclinical')[3]);
+    //   }).catch(function(error) {
+    //     alert('Error!');
+    //   });
+    // }
   });
