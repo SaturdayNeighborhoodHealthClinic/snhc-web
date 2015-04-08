fdb8abddfa192bc4c35ad25b6a9f3596d14fe618
Justin R. Porter <justinrporter@gmail.com>
2015-04-08 15:17:46 -0500

    Volunteering is now working

--- app/js/cal.js
+++ app/js/cal.js
@@ -36,71 +36,47 @@ angular.module('myApp.calcontrol', ['myApp.calService', 'firebase'])
     });
   })
   .controller('FirebaseCtrl', function($scope, $firebaseArray, $firebaseObject, fbRef, max_volunteers ){
-    $scope.firebase_event = $firebaseArray(fbRef.child("events").child($scope.event.id));
+    $scope.firebase_volunteers = $firebaseObject(fbRef.child("events").child($scope.event.id));
+    $scope.max_volunteers = max_volunteers;
+    $scope.uname = "bgunner";
 
     $scope.open = function(clinfilter) {
-      if( clinfilter == "ALL" ){
-        return true;
-      }
-
-      var volunteers = $scope.firebase_event.$getRecord(clinfilter);
-      if( !volunteers ){
-        // no volunteers for this event yet.
-        return true;
-      }
-
       var n_volunteers = 0;
+      // console.log( $scope.firebase_volunteers );
 
-      for( var volunteer in volunteers ){
+      for( var volunteer in $scope.firebase_volunteers[clinfilter] ){
         if(volunteer.charAt(0) != "$"){
           n_volunteers += 1;
         }
       }
 
       // use the max_volunteers value object to decide if this is an open event
-      return ( n_volunteers < max_volunteers[clinfilter] );
+      return ( n_volunteers < $scope.max_volunteers[clinfilter] );
     }
 
     $scope.volunteer = function(clinfilter){
-      var new_user = {"bgunner" : true };
-
-      console.log( $scope.firebase_event );
-
-      $scope.firebase_event[clinfilter]=new_user;
-
-      $scope.firebase_event.$save().then(function(ref) {
-        console.log( firebase_event );
-      })
-
-      // if( !(clinfilter in $scope.firebase_event ) ){
-      //   console.log( $firebaseObject(fbRef.child("events").child($scope.event.id)).$value );
-      //   var clin_grps = $firebaseObject(fbRef.child("events").child($scope.event.id));
-
-      //   // clin_grps.$value = { toString(clinfilter) : true };
-
-      //   // clin_grps.$save().then(function(ref) {
-      //   //   console.log( clin_grps.$value );
-      //   // }, function(error) {
-      //   //   console.log("Error:", error);
-      //   // });
-      // }
-      // var volunteers = $firebaseObject(fbRef.child("events").child($scope.event.id));
-
-      // if( !$scope.open(clinfilter) ){
-      //   return;
-      // } else if( clinfilter == "ALL" || clinfilter == "" ){
-      //   alert("You must select a role before volunteering.");
-      //   return;
-      // }
-
-      // if(!volunteers){        
-      //   $scope.firebase_event.$add({ clinfilter : new_user }).then( function(ref) {
-      //     var id = ref.key();
-      //     console.log( "added id "+id );
-      //     $scope.firebase_event.$save();
-      //   }).catch(function(error) {
-      //     alert("Authentication failed:", error);
-      //   });
-      // }
+      if( !$scope.open(clinfilter)){
+        alert( "This date doesn't look to be availiable. That's all we know." );
+        return;
+      } else if( $firebaseObject(fbRef.child("users")
+                                      .child($scope.uname)
+                                      .child("type")) != clinfilter ) {
+        alert( "You (username '" + $scope.uname + "') don't seem to be authorized to volunteer for this "+clinfilter+" spot." );
+        return;
+      } else {
+        var user_entry = $firebaseObject(fbRef.child("events")
+                                      .child($scope.event.id)
+                                      .child(clinfilter)
+                                      .child($scope.uname));
+        console.log( user_entry );
+        user_entry.$value = true;
+        console.log( user_entry );
+        user_entry.$save().then( function() {
+          console.log($firebaseObject(fbRef.child("events")
+                                              .child($scope.event.id)
+                                              .child(clinfilter)
+                                              .child($scope.uname)));
+        });
+      }
     }
   });
