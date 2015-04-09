d5b552f10c99b04c34bd65c401ff3ef4f0f79e74
Justin R. Porter <justinrporter@gmail.com>
2015-04-09 11:47:56 -0500

    Modify site to use angular-ui instead of bootstrap js.

--- app/app.js
+++ app/app.js
@@ -5,7 +5,7 @@ var myApp = angular.module('myApp', [
   'ngRoute',
   'myApp.routing',
   'myApp.datelist',
-  'myApp.version'
+  'ui.bootstrap'
 ]).
 config(['$routeProvider', function($routeProvider) {
   $routeProvider.otherwise({redirectTo: '/home'});
