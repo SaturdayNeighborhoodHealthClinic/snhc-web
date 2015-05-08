55bcbb81347fa358242b733fdfd2c5794b0252b1
Justin R. Porter <justinrporter@gmail.com>
2015-05-08 11:09:35 -0500

    Update update.sh to use GitHub org rather than justin's github.

--- update.sh
+++ update.sh
@@ -8,7 +8,7 @@ echo -n "UPDATE:"
 date
 
 rm -rf $( ls | grep -v bower_components | grep -v update.log | grep -v update.latest )
-wget --no-check-certificate https://github.com/justinrporter/snhc-web/archive/master.zip
+wget --no-check-certificate https://github.com/SaturdayNeghborhoodHealthClinic/snhc-web/archive/master.zip
 unzip master
 rm -r master
 mv snhc-web-master/app/* .
